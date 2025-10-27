a044c4311a118efbcc18c01bdb0b0f16fcae861f
amankumar6204 <amankumarjha6204@gmail.com>
2025-10-27 19:16:59 +0530

    update cloudinary config

--- server/src/config/cloudinary.js
+++ server/src/config/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: 'dgf9f6l71',
-  api_key: '928186646534965',
-  api_secret: 'UOdnt7TcASc2JBrqB6U4X7cHDBg',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
